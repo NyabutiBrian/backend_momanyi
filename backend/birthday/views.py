8e9be3ad2c80f2e3d96c5e56942a91c92b1b6dc2
Momanyi Poet <momanyiturner@gmail.com>
2024-03-30 19:16:06 +0300

    added django permission

--- backend/birthday/views.py
+++ backend/birthday/views.py
@@ -1,5 +1,3 @@
-from rest_framework.response import Response
-from django.shortcuts import render, get_object_or_404
 from rest_framework.viewsets import ModelViewSet
 from .models import Birthday
 from .serializers import BirthdaySerializer
